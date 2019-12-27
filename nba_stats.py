f6c5d3a3735789728226960a21c9cff287c0dab4
jirvingphd <james.irving.phd@gmail.com>
2019-12-26 22:25:00 -0500

    Added docstrings to functions

--- nba_stats.py
+++ nba_stats.py
@@ -4,47 +4,55 @@ import requests
 import lxml
 from pprint import pprint
 from bs4 import BeautifulSoup
-url = "https://stats.nba.com/player/1628369/boxscores-traditional/"
-
-# page = requests.get(url,'lxml')
-
-# soup = BeautifulSoup(page.content)
-# # pprint(soup.prettify()[:1000])
 from selenium import webdriver
 
-if __name__=='__main__':
-    driver = webdriver.Chrome()
+url = "https://stats.nba.com/player/1628369/boxscores-traditional/"
 
 
-    driver.get(url)
 
-    soup = BeautifulSoup(driver.page_source)
-
-    ## Get all table rows
-    row_tags = soup.find_all('tr')
-
-    ## Get header/column names
-    head = row_tags[0]
-    column_names = [t.text for t in head.find_all('th')]
-
-    data = []
-    for row in row_tags[1:]:
-        row_data = [t.text.strip('\n').strip() for t in row.find_all('td')]
-        data.append(row_data)
+# if __name__=='__main__':
+#     driver = webdriver.Chrome()
 
 
-    df = pd.DataFrame(data,columns=column_names)
+#     driver.get(url)
 
-    float_cols= [col for col in df.columns if '%' in col]
-    obj_cols= ['Match Up','W/L']
-    int_cols = [col for col in df.columns if (col not in float_cols) and (col not in obj_cols)]
-    df.dropna(inplace=True)
+#     soup = BeautifulSoup(driver.page_source)
 
-    df[int_cols] = df[int_cols].astype('int')
-    df[float_cols] = df[float_cols].astype('float')
+#     ## Get all table rows
+#     row_tags = soup.find_all('tr')
+
+#     ## Get header/column names
+#     head = row_tags[0]
+#     column_names = [t.text for t in head.find_all('th')]
+
+#     data = []
+#     for row in row_tags[1:]:
+#         row_data = [t.text.strip('\n').strip() for t in row.find_all('td')]
+#         data.append(row_data)
+
+
+#     df = pd.DataFrame(data,columns=column_names)
+
+#     float_cols= [col for col in df.columns if '%' in col]
+#     obj_cols= ['Match Up','W/L']
+#     int_cols = [col for col in df.columns if (col not in float_cols) and (col not in obj_cols)]
+#     df.dropna(inplace=True)
+
+#     df[int_cols] = df[int_cols].astype('int')
+#     df[float_cols] = df[float_cols].astype('float')
 
 
 def get_season_df(url,driver=None):
+    """
+    Loads the provided url. Intended for: stats.nba.com/player/{player_id}}/boxscores-traditional/)
+    Helper Function to `get_all_seasons_df`, but can be used alone.
+    Args:
+        url (str): nba stats plaer boxscore-traditional page. 
+        driver (Chrome webdrive), optional): Selenium webdriver object. Created if not provided. Defaults to None.
+    
+    Returns:
+        df (Frame): extracted data table from stats.nba.com
+    """
     from selenium import webdriver
     import time
     import pandas as pd
@@ -88,6 +96,18 @@ def get_season_df(url,driver=None):
 
 
 def make_season_urls_from_playerid(player_id='1628369',season_years=[2019,2018,2017]):
+    """
+    Creates the correct urls for each of the season years programmatically.
+    base_url = f"https://stats.nba.com/player/{player_id}/boxscores-traditional/"
+    season_suffix = f"?Season={start_YR}-{end_yr}&SeasonType=Regular%20Season"
+
+    Args:
+        player_id (str, optional): stats.nba.com player id for url. Defaults to '1628369'.
+        season_years (list of ints, optional): List of years to extract. Only 3 seasons available. Defaults to [2019,2018,2017].
+    
+    Returns:
+        season_urls (list): List of urls created 
+    """
     ## SEPARATING ABOVE INTO F_STRING 
     if isinstance(player_id,int):
         player_id = str(player_id)
@@ -95,7 +115,6 @@ def make_season_urls_from_playerid(player_id='1628369',season_years=[2019,2018,2
     base_url = f"https://stats.nba.com/player/{player_id}/boxscores-traditional/"
 
 
-    seasons= {}
     season_urls = []
 
     for year in season_years:
@@ -103,16 +122,31 @@ def make_season_urls_from_playerid(player_id='1628369',season_years=[2019,2018,2
         start_YR = str(year)
         end_yr = str(year+1)[-2:]
         
-        # seasons[year] = dict()
         season_suffix = f"?Season={start_YR}-{end_yr}&SeasonType=Regular%20Season"
+
         full_url = base_url+season_suffix
         season_urls.append(full_url)
+
     return season_urls
 
 
 
     
 def get_all_seasons_df(season_urls=None,player_id=None,season_years=[2019,2018,2017]):
+    """
+    Extract data from all season urls provided and concatenate into one dataframe.
+    Args:
+        season_urls (list of str, optional): List of urls to extract from. If None, generate season_urls from player_id.
+        player_id (str or int, optional): stats.nba.com player_id for urls. Defaults to None.
+        season_years (list, optional): [description]. Defaults to [2019,2018,2017].
+    
+    Raises:
+        Exception: Must provided EITHER list of season_urls
+            or player_id & season_years to generate them with `make_season_urls_from_playerid`
+    
+    Returns:
+        df_combined (Frame): df with all seasons for provided urls/player_id
+    """
     import pandas as pd
     import time
     if (season_urls is None):
@@ -171,10 +205,22 @@ def calc_fantasy_stats(df,pts=None):
     return df_fantasy
 
     
-def save_player_df(df,player_id=None):
+def save_player_df(df,player_id=None,verbose=True):
+    """
+    Saves the player df to the data/players/subfolder 
+    using the player_id as filename suffix. 
+    
+    Args:
+        df (Frame): player's df to save
+        player_id (str, optional): Player_id for filename. Defaults to None, 
+            which extracts it from the df..
+    """
     if player_id is None:
         player_id=df['player_id'][0]
         
     filename=f"data/players/player_id_{player_id}.csv"
     df.to_csv(filename)
+    
+    if verbose:
+        print(f"[i] df saved as {filename}.")
             
